4ff95e78dc438270374f6f80459bde82f7d01c45
monstersprites <49540368+monstersprites@users.noreply.github.com>
2021-03-20 21:41:52 -0400

    Add files via upload
    Dependencias x2

--- Tarea8/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/FetchData.razor.g.cs
+++ Tarea8/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/FetchData.razor.g.cs
@@ -77,6 +77,34 @@ using Tarea8.Shared;
 #line hidden
 #nullable disable
 #nullable restore
+#line 10 "C:\Users\Daniel Lora\Desktop\Blazor's Apps\Tarea8\_Imports.razor"
+using Newtonsoft.Json;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 11 "C:\Users\Daniel Lora\Desktop\Blazor's Apps\Tarea8\_Imports.razor"
+using System.Net;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 12 "C:\Users\Daniel Lora\Desktop\Blazor's Apps\Tarea8\_Imports.razor"
+using Radzen;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 13 "C:\Users\Daniel Lora\Desktop\Blazor's Apps\Tarea8\_Imports.razor"
+using Radzen.Blazor;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
 #line 3 "C:\Users\Daniel Lora\Desktop\Blazor's Apps\Tarea8\Pages\FetchData.razor"
 using Tarea8.Data;
 
--- Tarea8/obj/Debug/netcoreapp3.1/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ Tarea8/obj/Debug/netcoreapp3.1/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -76,6 +76,34 @@ using Tarea8.Shared;
 #line default
 #line hidden
 #nullable disable
+#nullable restore
+#line 10 "C:\Users\Daniel Lora\Desktop\Blazor's Apps\Tarea8\_Imports.razor"
+using Newtonsoft.Json;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 11 "C:\Users\Daniel Lora\Desktop\Blazor's Apps\Tarea8\_Imports.razor"
+using System.Net;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 12 "C:\Users\Daniel Lora\Desktop\Blazor's Apps\Tarea8\_Imports.razor"
+using Radzen;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 13 "C:\Users\Daniel Lora\Desktop\Blazor's Apps\Tarea8\_Imports.razor"
+using Radzen.Blazor;
+
+#line default
+#line hidden
+#nullable disable
     public partial class MainLayout : LayoutComponentBase
     {
         #pragma warning disable 1998
